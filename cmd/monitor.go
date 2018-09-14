105a190faf7590f9761d2dcebfad4e1a133a7d98
John Kamenik <jkamenik@fourv.com>
2018-09-14 16:41:05 -0400

    Implement a simple monitor.

--- cmd/monitor.go
+++ cmd/monitor.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
+	"time"
 
+	"github.com/jkamenik/log-pruner/util"
 	"github.com/spf13/cobra"
 )
+
+var wait int
 
 // monitorCmd represents the monitor command
 var monitorCmd = &cobra.Command{
@@ -12,12 +16,22 @@ var monitorCmd = &cobra.Command{
 	Short: "Like prune but runs forever",
 	Long:  `Continually scans and prunes all the paths provided.  When done it sleeps for a time and then starts again.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("monitor called")
+		for true {
+			for _, path := range allSettings.paths {
+				err := prune(path, allSettings.fileAgeMax, util.BytesFromGb(allSettings.pathTargetSize), allSettings.dryRun)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			log.Printf("Done Pruning, waiting %ds for next prune cycle", wait)
+			time.Sleep(time.Duration(wait) * time.Second)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(monitorCmd)
 
-	monitorCmd.Flags().Int("wait", 300, "Wait time in seconds between successive checks.  Note: all directories are scanned and pruned entirely before the wait is triggered.")
+	monitorCmd.Flags().IntVar(&wait, "wait", 300, "Wait time in seconds between successive checks.  Note: all directories are scanned and pruned entirely before the wait is triggered.")
 }
